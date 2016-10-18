afbd7e2cd4f7adb49b726dba457558ca34cc0083
ShinRa26 <grahamkeenan@hotmail.co.uk>
2016-10-18 09:58:05 +0100

    Minor refactoring

--- src/PyExe/Program.java
+++ src/PyExe/Program.java
@@ -1,5 +1,5 @@
 package PyExe;
-import java.io.*;
+
 /**
  * Launches the applicaiton
  * @author Graham Keenan (GAK)
@@ -9,30 +9,7 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-		
+		//Starts the GUI
 		GUI g = new GUI();
-		
-		/*
-		try
-		{
-			Process p = null;
-			p = Runtime.getRuntime().exec("cmd /C pyinstaller \"C:\\Users\\graha\\Desktop\\Main.py\" --clean --onefile --distpath C:\\Users\\graha\\Desktop\\");
-			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader err = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			
-			String line = "";
-			
-			while((line = r.readLine()) != null)
-				System.out.println(line);
-			
-			while((line = err.readLine()) != null)
-				System.out.println(line);
-			
-			
-			if( p!= null)
-				p.waitFor();
-		}
-		catch(Exception e){}
-		*/
 	}
 }
